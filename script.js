85fe6a4f2c44b2dfa2ac4a24453025eda0b35c59
Lucas <lucas.konzen1@gmail.com>
2022-11-06 20:38:14 -0300

    commit port

---
+++ script.js
@@ -0,0 +1,16 @@
+var btn = document.querySelector('.logo')
+var container = document.querySelector('.container')
+var link = document.querySelector('.link')
+
+btn.addEventListener('click', function() {
+
+    if(container.style.visibility = 'hidden'){
+        container.style.visibility = 'visible'
+    } else {
+        container.style.visibility ='hidden'
+    }
+  
+    
+})
+
+
