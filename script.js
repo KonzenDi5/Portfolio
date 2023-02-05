40b6faea2c09064df434e69e8d350c2a039a4267
Lucas Konzen <104283188+KonzenDi5@users.noreply.github.com>
2023-02-05 15:27:58 -0300

    Commit

--- script.js
+++ script.js
@@ -1,10 +1,10 @@
-var btn = document.querySelector('.sobre');
-var container = document.querySelector('.eu');
-btn.addEventListener('click', function() {
+
+var container = document.querySelector(".eu");
+
+function revealMe(){
     
-  if(container.style.height === "0") {
+  if(container.style.height == 0) {
       container.style.height = "600px";
-  } else {
-      container.style.height = "0";
   }
-})
+else {container.style.height = 0
+}}
